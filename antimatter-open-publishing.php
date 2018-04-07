e36c600e3aa4e9ebc650c7590255511b5a8310a9
Paul Hibbitts <paul_hibbitts@sfu.ca>
2018-04-06 17:16:08 -0700

    Added support for theme-based ShortCodes.

--- antimatter-open-publishing.php
+++ antimatter-open-publishing.php
@@ -5,5 +5,26 @@ use Grav\Common\Theme;
 
 class AntimatterOpenPublishing extends Antimatter
 {
+
+  public static function getSubscribedEvents()
+  {
+      return [
+          'onShortcodeHandlers' => ['onShortcodeHandlers', 0],
+          'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+      ];
+  }
+
+  public function onShortcodeHandlers()
+  {
+      $this->grav['shortcode']->registerAllShortcodes('theme://shortcodes');
+  }
+
+  public function onTwigSiteVariables()
+  {
+      if ($this->isAdmin() && ($this->grav['config']->get('plugins.shortcode-core.enabled'))) {
+          $this->grav['assets']->add('theme://editor-buttons/admin/js/shortcode-h5p.js');
+      }
+  }
+
 }
 ?>
